f49194430763755b8cb11d40ca2135d7966d0bfd
pablo.coronel <pocoronel@gmail.com>
2021-11-17 09:35:44 -0300

    commiteando desafio 2

---
+++ src/components/Nav-Bs/NavBar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/#">
+            NavBar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link active" aria-current="page" href="/#">
+                INICIO
+              </a>
+              <a className="nav-link" href="/#">
+                Microprocesadores
+              </a>
+              <a className="nav-link" href="/#">
+                Placas de video
+              </a>
+              <a className="nav-link" href="/#">
+                Memorias Ram
+              </a>
+              <a className="nav-link" href="/#">
+                Mothers
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.scss'
+
+const Nav = () => {
+    return (
+        <div>
+            <ul className="navContainer">
+                <li className="list">Home</li>
+                <li className="list">Categoría A</li>
+                <li className="list">Categoría B</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
