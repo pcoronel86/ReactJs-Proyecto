a8a3ce746244a9dd866796dfa485639f0164a9fd
pablo.coronel <pocoronel@gmail.com>
2021-12-16 22:36:59 -0300

    Desafio 7 Sincronizar Counter 2 entrega

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ItemListContainer from './Containers/ItemListContainer/ItemListContainer'
 import NavBar from './components/Nav-Bs/NavBar';
 import ItemDetailContainer from './Containers/ItemDetailContainer/ItemDetailContainer';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import { NotFound } from 'http-errors';
+import Cart from './components/Cart/Cart';
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
             <Route path='/' element={<ItemListContainer/>}/>
             <Route path='/category/:categoryId' element={<ItemListContainer/>} />
             <Route path='/item/:paramId' element={<ItemDetailContainer />} />
-            <Route path="*" element={<NotFound/>}/>
+            <Route path='cart' element={<Cart/>}/>  
           </Routes>
       </BrowserRouter>
     </div>
