6e80a16105400cc66f143e0854dca3d60798e7aa
pablo.coronel <pocoronel@gmail.com>
2021-12-30 00:24:05 -0300

    co

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,61 +1,153 @@
-import { useContext } from 'react';
-import { Icon } from 'semantic-ui-react';
-import { CartContext } from '../../Context/CartContext';
-import { Link } from 'react-router-dom';
+import { useContext, useRef, useState } from "react";
+import { Icon } from "semantic-ui-react";
+import { CartContext } from "../../Context/CartContext";
+import { Link } from "react-router-dom";
+import { addDoc, collection, Timestamp, doc, writeBatch, getDoc} from 'firebase/firestore'
+import { db } from "../../sevices/firebase/firebase";
+
+
 
 const Cart = () => {
-    const {obtenerTotal} = useContext(CartContext);
-    const {cart} = useContext (CartContext);
-    const {removeItem} = useContext(CartContext)
-    const {cleanCart}= useContext(CartContext)
+  const [processingOrder, setProcessingOrder] = useState(false)
+  const [contact, setContact] = useState({
+    name:'',
+    phone:'',
+    address:'',
+  })
+  const { obtenerTotal } = useContext(CartContext);
+  const { products } = useContext(CartContext);
+  const { removeItem } = useContext(CartContext);
+  const { cleanCart } = useContext(CartContext);
+  const contactFormRef = useRef ()
 
-    const removeAll=()=>{
-        cleanCart()
+  const confirmOrder = () => {
+    setProcessingOrder(true)
+
+    const objOrder = {
+        buyer: contact.name,
+        items: products,
+        total: obtenerTotal(),
+        phone: contact.phone,
+        address: contact.address, 
+        date: Timestamp.fromDate(new Date())
     }
-    
-    return (
+
+    const batch = writeBatch(db)
+    const outOfStock = []
+
+    objOrder.items.forEach((prod) => {
+        getDoc(doc(db, 'items'), prod.id).then((documentSnapshot)=>{
+          if(documentSnapshot.data().stock>= prod.cantidad){
+            batch.update(doc(db, 'items', documentSnapshot.id),{
+              stock : documentSnapshot.data().stock - prod.cantidad
+            })
+          }else{
+              outOfStock.push({id: documentSnapshot.id, ...documentSnapshot.data()})
+          }
+        })
+    })
+
+    if(outOfStock.length === 0){
+      addDoc(collection(db, 'orders'), objOrder).then(({id}) =>{
+        console.log(id)
+        
+      })
+    }
+    addDoc(collection(db, 'orders'), objOrder).then(({id}) =>{
+      console.log(id)
+
+    setTimeout(() =>{
+      cleanCart()
+      setProcessingOrder(false)
+    }, 1000)
+  }
+  
+  const removeAll = () => {
+    cleanCart();
+  };
+
+  return (
+    <div>
+    {obtenerTotal() === 0 ? (
+      <div>
+        <h1>Carrito de Compras Vacio</h1>
         <div>
-        {obtenerTotal() === 0 ?
+          <Link
+            to="/"
+            className="btn btn-success ml-auto comprarButton"
+            type="button"
+            data-toggle="modal"
+            data-target="#comprarModal"
+          >
+            {" "}
+            Return{" "}
+          </Link>
+        </div>
+      </div>
+    ) : (
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Foto</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Cantidad</th>
+            <th scope="col">Precio Unidad</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => {
+            return (
+              <tr>
+                <td>
+                  <img src={product.item.pictureURL} alt="" />
+                </td>
+                <td> {product.item.title} </td>
+                <td> {product.cantidad} </td>
+                <td>{product.item.price}</td>
+                <button
+                  color="red"
+                  onClick={(handleRemove) => removeItem(product.item.id)}
+                >
+                  <Icon name="trash" />
+                </button>
+              </tr>
+            );
+          })}
+        </tbody>
         <div>
-            <h1>
-                Carrito de Compras Vacio
-            </h1>
-            <div>
-            <Link to = '/' className="btn btn-success ml-auto comprarButton" type="button" data-toggle="modal" data-target="#comprarModal"> Return </Link>
-            </div>
+          <h1>TOTAL:${obtenerTotal()}</h1>
         </div>
-        :
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                <th scope="col">Foto</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Cantidad</th>
-                <th scope="col">Precio Unidad</th>
-                </tr>
-            </thead>
-            <tbody>
-                {cart.map(product => {
-                        return <tr> 
-                            <td><img src={product.item.pictureURL} alt="" /></td>
-                            <td> {product.item.title} </td>
-                            <td> {product.cantidad} </td>
-                            <td>{product.item.price}</td>
-                            <button color="red" onClick={(handleRemove)=>removeItem(product.item.id)}><Icon name="trash"/></button>
-                        </tr>
-                        
-            })}
-            </tbody>
-            <div>
-                <h1>TOTAL:${obtenerTotal()}</h1>
-            </div>
-            <div>
-            <button className="btn btn-success ml-auto comprarButton" type="button" data-toggle="modal" data-target="#comprarModal"
-            onClick={removeAll}>Remove All</button>
-            </div>
-        </table>}
+        <div>
+          <button
+            className="btn btn-success ml-auto comprarButton"
+            type="button"
+            data-toggle="modal"
+            data-target="#comprarModal"
+            onClick={removeAll}
+          >
+            Remove All
+          </button>
+          <button
+            className="btn btn-success ml-auto comprarButton"
+            type="button"
+            data-toggle="modal"
+            data-target="#comprarModal"
+            onClick={()=>confirmOrder()}
+          >
+            Confirmar Compra
+          </button>
         </div>
-    )
-}
+            <div>
+              <h4>Nombre: {contact.name}</h4>
+              <h4>Telefono: {contact.phone}</h4>
+              <h4>Direccion: {contact.address}</h4>
+              <button onClick={() => setContact({name:'', phone:'', address:'', comment:''})}>Agregar Contacto</button>
+            </div>
+            
+      </table>
+    )}
+  </div>
+  )
+};
 
 export default Cart;
